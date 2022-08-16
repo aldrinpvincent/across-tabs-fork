98a1d34e2ea0e106d0fd8f7c9d87415b3d6ed190
aldrinpvincent-moengage <aldrin.vincet@moengage.com>
2022-08-16 22:26:56 +0530

    updating dependancies

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var createVariants = require('parallel-webpack').createVariants;
+const path = require('path');
 
-// Import the plugin:
-var argv = require('yargs').argv.env;
-var env = argv.mode;
+const webpack = require('webpack');
+// var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+// var createVariants = require('parallel-webpack').createVariants;
 
-console.warn(env);
+// // Import the plugin:
+// var argv = require('yargs').argv.env;
+// var env = argv.mode;
 
-var libraryName = 'across-tabs';
+// console.warn(env);
 
-var libVersion = JSON.stringify(require('./package.json').version);
+// var libraryName = 'across-tabs';
 
-var libraryHeaderComment =
+const libVersion = JSON.stringify(require('./package.json').version);
+
+const libraryHeaderComment =
   '\n' +
   'across-tabs ' +
   libVersion +
@@ -29,60 +31,73 @@ var plugins = [
   })
 ];
 
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-}
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  plugins: plugins,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'across-tabs.js',
+    library: {
+      name: 'AcrossTabs',
+      type: 'umd'
+    }
+  }
+};
+// if (env === 'build') {
+//   plugins.push(new UglifyJsPlugin({ minimize: true }));
+// }
 
-function createConfig(options) {
-  return {
-    entry: __dirname + '/src/index.js',
-    devtool: 'source-map',
-    output: {
-      path: __dirname + '/dist',
-      library: 'AcrossTabs',
-      filename: libraryName + (options.target ? '.' + options.target : '') + (env === 'build' ? '.min.js' : '.js'),
-      libraryTarget: options.target || 'umd',
-      umdNamedDefine: true
-    },
-    module: {
-      rules: [
-        {
-          test: /(\.js)$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            // babel-loader to convert ES6 code to ES5 + amdCleaning requirejs code into simple JS code, taking care of modules to load as desired
-            loader: 'babel-loader',
-            options: {
-              presets: ['env'],
-              plugins: []
-            }
-          }
-        },
-        {
-          enforce: 'pre',
-          test: /(\.js)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-              emitError: true,
-              emitWarning: true,
-              failOnWarning: env === 'build',
-              failOnError: env === 'build'
-            }
-          }
-        }
-      ]
-    },
-    plugins: plugins
-  };
-}
+// function createConfig(options) {
+//   return {
+//     entry: __dirname + '/src/index.js',
+//     devtool: 'source-map',
+//     output: {
+//       path: __dirname + '/dist',
+//       library: 'AcrossTabs',
+//       filename: libraryName + (options.target ? '.' + options.target : '') + (env === 'build' ? '.min.js' : '.js'),
+//       libraryTarget: options.target || 'umd',
+//       umdNamedDefine: true
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /(\.js)$/,
+//           exclude: /(node_modules|bower_components)/,
+//           use: {
+//             // babel-loader to convert ES6 code to ES5 + amdCleaning requirejs code into simple JS code, taking care of modules to load as desired
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['env'],
+//               plugins: []
+//             }
+//           }
+//         },
+//         {
+//           enforce: 'pre',
+//           test: /(\.js)$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'eslint-loader',
+//             options: {
+//               fix: true,
+//               emitError: true,
+//               emitWarning: true,
+//               failOnWarning: env === 'build',
+//               failOnError: env === 'build'
+//             }
+//           }
+//         }
+//       ]
+//     },
+//     plugins: plugins
+//   };
+// }
 
-// At the end of the file:
-module.exports = createVariants(
-  {
-    target: ['this', '']
-  },
-  createConfig
-);
+// // At the end of the file:
+// module.exports = createVariants(
+//   {
+//     target: ['this', '']
+//   },
+//   createConfig
+// );
